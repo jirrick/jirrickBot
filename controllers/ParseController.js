677128c8cf9bd102812e3f0a5495e97e688c15cb
jirrick <jurahudec@gmail.com>
2017-08-21 18:32:26 +0200

    return bool value from parsing methods

--- controllers/ParseController.js
+++ controllers/ParseController.js
@@ -4,10 +4,11 @@ const config = require('../config'),
 	botName = config.twitch_channel.bot;
 
 exports.parse = function (msg) {
-	Spin(msg);
+	if(Spin(msg)) return;
 };
 
 function Spin(msg) {
+	let result = false;
 	//Prepare regexp
 	const regexp = /([\w\d]+) (won|lost) ([\d,]+)/;
 	const match = regexp.exec(msg.message);
@@ -15,8 +16,10 @@ function Spin(msg) {
 		//Parse results
 		const nick = match[1];
 		let amount = parseInt(match[3].replace(',', ''));
-		if (match[2] == 'lost') amount *= -1;
+		if (match[2] === 'lost') amount *= -1;
 		//TODO Process results
 		if (logging) console.log(`SPIN: ${nick} [${amount}]`);
+		result = true;
 	}
+	return result;
 }
